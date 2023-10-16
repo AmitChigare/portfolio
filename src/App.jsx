0732a6a9982ce451309b11cf62ebae92a26e1597
AmitChigare <amit.gokakfalls17@gmail.com>
2023-10-16 10:25:20 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./app.scss";
+
+const App = () => {
+  return (
+    <div>
+      <section>Hero</section>
+      <section>Parallax</section>
+      <section>Services</section>
+      <section>Parallax</section>
+      <section>Portfoli0 1 </section>
+      <section>Portfolio 2</section>
+      <section>Portfolio 3</section>
+      <section>Contact</section>
+    </div>
+  );
+};
+
+export default App;
