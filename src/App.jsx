18879f97bc049f5a78604cc9df2e5289757889ae
AmitChigare <amit.gokakfalls17@gmail.com>
2023-10-16 10:46:06 +0530

    Implemeted Navbar and Sidebar componets

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
 import "./app.scss";
 
+import Navbar from "./components/Navbar/Navbar";
+
 const App = () => {
   return (
     <div>
-      <section>Hero</section>
-      <section>Parallax</section>
-      <section>Services</section>
-      <section>Parallax</section>
-      <section>Portfoli0 1 </section>
-      <section>Portfolio 2</section>
-      <section>Portfolio 3</section>
-      <section>Contact</section>
+      <section id="Homepage">
+        <Navbar />
+      </section>
     </div>
   );
 };
